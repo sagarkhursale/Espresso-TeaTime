e13b653a2c7a5b9fd38e4d736214da47f153dc1e
sagarkhursale <khursule.sagar@gmail.com>
2019-01-21 18:20:20 +0530

    idling resource

--- app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -1,22 +1,21 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.teatime;
 
-
+import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -27,15 +26,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * Usually Espresso syncs all view operations with the UI thread as well as AsyncTasks, but it can't
  * do so with custom resources (e.g. activity or service). For such cases, we can register the
  * custom resource and Espresso will wait for the resource to be idle before
  * executing a view operation.
- *
+ * <p>
  * In this example, we simulate an idling situation. This test is the same as the
  * MenuActivityScreenTest but with an Idling Resource to help with synchronization.
- *
+ * <p>
  * We added an idling period from when the user clicks on a GridView item
  * in MenuActivity to when corresponding order activity appears. This is to simulate potential
  * delay that could happen if this data were being retrieved from the web. Without registering the
@@ -51,7 +55,7 @@ public class IdlingResourceMenuActivityTest {
      * The ActivityTestRule is a rule provided by Android used for functional testing of a single
      * activity. The activity that will be tested, MenuActivity in this case, will be launched
      * before each test that's annotated with @Test and before methods annotated with @Before.
-     *
+     * <p>
      * The activity will be terminated after the test and methods annotated with @After are
      * complete. This rule allows you to directly access the activity during the test.
      */
@@ -59,25 +63,31 @@ public class IdlingResourceMenuActivityTest {
     public ActivityTestRule<MenuActivity> mActivityTestRule =
             new ActivityTestRule<>(MenuActivity.class);
 
+
     private IdlingResource mIdlingResource;
 
 
     // TODO (6) Registers any resource that needs to be synchronized with Espresso before
-    // the test is run.
     @Before
     public void registerIdlingResource() {
-
+        // the test is run.
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        IdlingRegistry.getInstance().register(mIdlingResource);
     }
 
     // TODO (7) Test that the gridView with Tea objects appears and we can click a gridView item
     @Test
     public void idlingResourceTest() {
-
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
     }
 
     // TODO (8) Unregister resources when not needed to avoid malfunction
     @After
     public void unregisterIdlingResource() {
-
+        if (mIdlingResource != null) {
+            IdlingRegistry.getInstance().unregister(mIdlingResource);
+        }
     }
+
+// END
 }
--- app/src/main/java/com/example/android/teatime/MenuActivity.java
+++ app/src/main/java/com/example/android/teatime/MenuActivity.java
@@ -1,61 +1,62 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.teatime;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.example.android.teatime.IdlingResource.SimpleIdlingResource;
 import com.example.android.teatime.model.Tea;
 
 import java.util.ArrayList;
 
 // TODO (1) Implement ImageDownloader.DelayerCallback
-public class MenuActivity extends AppCompatActivity {
-
-    Intent mTeaIntent;
+public class MenuActivity extends AppCompatActivity implements ImageDownloader.DelayerCallback {
 
     public final static String EXTRA_TEA_NAME = "com.example.android.teatime.EXTRA_TEA_NAME";
 
+    Intent mTeaIntent;
+    GridView gridview;
+
     // TODO (2) Add a SimpleIdlingResource variable that will be null in production
+    public SimpleIdlingResource mIdlingResource;
 
     /**
      * TODO (3) Create a method that returns the IdlingResource variable. It will
      * instantiate a new instance of SimpleIdlingResource if the IdlingResource is null.
      * This method will only be called from test.
      */
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
 
-
-    /**
-     * TODO (4) Using the method you created, get the IdlingResource variable.
-     * Then call downloadImage from ImageDownloader. To ensure there's enough time for IdlingResource
-     * to be initialized, remember to call downloadImage in either onStart or onResume.
-     * This is because @Before in Espresso Tests is executed after the activity is created in
-     * onCreate, so there might not be enough time to register the IdlingResource if the download is
-     * done too early.
-     */
-
-
-    // TODO (5) Override onDone so when the thread in ImageDownloader is finished, it returns an
-    // ArrayList of Tea objects via the callback.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,10 +77,17 @@ public class MenuActivity extends AppCompatActivity {
 
         // Create a {@link TeaAdapter}, whose data source is a list of {@link Tea}s.
         // The adapter know how to create grid items for each item in the list.
-        GridView gridview = (GridView) findViewById(R.id.tea_grid_view);
-        TeaMenuAdapter adapter = new TeaMenuAdapter(this, R.layout.grid_item_layout, teas);
-        gridview.setAdapter(adapter);
+        gridview = findViewById(R.id.tea_grid_view);
 
+        /**
+         * TODO (4) Using the method you created, get the IdlingResource variable.
+         * Then call downloadImage from ImageDownloader. To ensure there's enough time for IdlingResource
+         * to be initialized, remember to call downloadImage in either onStart or onResume.
+         * This is because @Before in Espresso Tests is executed after the activity is created in
+         * onCreate, so there might not be enough time to register the IdlingResource if the download is
+         * done too early.
+         */
+        getIdlingResource();
 
         // Set a click listener on that View
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -95,4 +103,22 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ImageDownloader.downloadImage(this, MenuActivity.this, mIdlingResource);
+    }
+
+
+    // TODO (5) Override onDone so when the thread in ImageDownloader is finished, it returns an
+    @Override
+    public void onDone(ArrayList<Tea> teas) {
+        // ArrayList of Tea objects via the callback.
+        TeaMenuAdapter adapter = new TeaMenuAdapter(this, R.layout.grid_item_layout, teas);
+        gridview.setAdapter(adapter);
+    }
+
+    // END
 }
